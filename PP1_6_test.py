3cdfa26d930234ac9c4495f16d3487157059b3db
MrKalisz <72149056+MrKalisz@users.noreply.github.com>
2024-09-23 20:29:49 -0400

    Update PP1_6_test.py

--- PP1_6_test.py
+++ PP1_6_test.py
@@ -112,7 +112,7 @@ def test_q2_2(capsys):
 		return input_values.pop(0)
 	PP1_6.input = mock_input
 
-	PP1_6.q6()
+	PP1_6.q2()
 	captured = capsys.readouterr()
 	assert captured.out == "Input a number: Input a number: 1\n"
 
@@ -131,7 +131,7 @@ def test_q3_2(capsys):
 		return input_values.pop(0)
 	PP1_6.input = mock_input
 
-	PP1_6.q7()
+	PP1_6.q3()
 	captured = capsys.readouterr()
 	assert captured.out == "hello Mr. Kalisz have you seen my work yet?\n"
 
@@ -150,7 +150,7 @@ def test_q4_2(capsys):
 		return input_values.pop(0)
 	PP1_6.input = mock_input
 
-	PP1_6.q8()
+	PP1_6.q4()
 	captured = capsys.readouterr()
 	assert captured.out == "Input a number: Input a number: 7\n"
 
