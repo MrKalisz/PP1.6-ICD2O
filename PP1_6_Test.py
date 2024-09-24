e08b4a6159221e0e9a39b1ca27367d447e916a87
MrKalisz <72149056+MrKalisz@users.noreply.github.com>
2024-09-23 20:26:34 -0400

    Add files via upload

---
+++ PP1_6_Test.py
@@ -0,0 +1,156 @@
+import os.path
+import sys
+import PP1_6
+
+def test_q1_1(capsys):
+
+	try:
+		exists = os.path.exists("{fileName}.py")
+		assert exists
+	except:
+		sys.exit()
+
+	input_values = [5]
+
+	def mock_input(s):
+		print(s, end='')
+		return input_values.pop(0)
+	PP1_6.input = mock_input
+
+	PP1_6.q1()
+	captured = capsys.readouterr()
+	assert captured.out == "Input a number: Input a number: 14\n"
+
+def test_q2_1(capsys):
+
+	try:
+		exists = os.path.exists("{fileName}.py")
+		assert exists
+	except:
+		sys.exit()
+
+	input_values = [9]
+
+	def mock_input(s):
+		print(s, end='')
+		return input_values.pop(0)
+	PP1_6.input = mock_input
+
+	PP1_6.q2()
+	captured = capsys.readouterr()
+	assert captured.out == "Input a number: Input a number: 5\n"
+
+def test_q3_1(capsys):
+
+	try:
+		exists = os.path.exists("{fileName}.py")
+		assert exists
+	except:
+		sys.exit()
+
+	input_values = [55]
+
+	def mock_input(s):
+		print(s, end='')
+		return input_values.pop(0)
+	PP1_6.input = mock_input
+
+	PP1_6.q3()
+	captured = capsys.readouterr()
+	assert captured.out == "hello Mr. Kalisz have you seen my work yet?\n"
+
+def test_q4_1(capsys):
+
+	try:
+		exists = os.path.exists("{fileName}.py")
+		assert exists
+	except:
+		sys.exit()
+
+	input_values = [10]
+
+	def mock_input(s):
+		print(s, end='')
+		return input_values.pop(0)
+	PP1_6.input = mock_input
+
+	PP1_6.q4()
+	captured = capsys.readouterr()
+	assert captured.out == "Input a number: Input a number: 24\n"
+
+def test_q5_1(capsys):
+
+	try:
+		exists = os.path.exists("{fileName}.py")
+		assert exists
+	except:
+		sys.exit()
+
+	input_values = [11]
+
+	def mock_input(s):
+		print(s, end='')
+		return input_values.pop(0)
+	PP1_6.input = mock_input
+
+	PP1_6.q5()
+	captured = capsys.readouterr()
+	assert captured.out == "Input a number: Input a number: 6\n"
+
+def test_q6_1(capsys):
+
+	try:
+		exists = os.path.exists("{fileName}.py")
+		assert exists
+	except:
+		sys.exit()
+
+	input_values = [2.2]
+
+	def mock_input(s):
+		print(s, end='')
+		return input_values.pop(0)
+	PP1_6.input = mock_input
+
+	PP1_6.q6()
+	captured = capsys.readouterr()
+	assert captured.out == "Input a number: Input a number: 1\n"
+
+def test_q1_2(capsys):
+
+	try:
+		exists = os.path.exists("{fileName}.py")
+		assert exists
+	except:
+		sys.exit()
+
+	input_values = [2]
+
+	def mock_input(s):
+		print(s, end='')
+		return input_values.pop(0)
+	PP1_6.input = mock_input
+
+	PP1_6.q7()
+	captured = capsys.readouterr()
+	assert captured.out == "hello Mr. Kalisz have you seen my work yet?\n"
+
+def test_q2_2(capsys):
+
+	try:
+		exists = os.path.exists("{fileName}.py")
+		assert exists
+	except:
+		sys.exit()
+
+	input_values = [4]
+
+	def mock_input(s):
+		print(s, end='')
+		return input_values.pop(0)
+	PP1_6.input = mock_input
+
+	PP1_6.q8()
+	captured = capsys.readouterr()
+	assert captured.out == "Input a number: Input a number: 7\n"
+
